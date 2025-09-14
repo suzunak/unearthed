8528c9e50bb4a980dea349acd200a4d1abba1351
suzunak <suzunakimura@gmail.com>
2025-09-14 18:05:00 -0400

    Create files and route for gifts

---
+++ unearthed/server/routes/gifts.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import giftData from '../data/gifts.js'
+
+// naming convention __ to indicate that this is not meant to be overwritten or redefined
+// import.meta.url --> special property that contains the URL to the current module file
+const __filename = fileURLToPath(import.meta.url); //stores results
+const __dirname = path.dirname(__filename);
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.status(200).json(giftData);
+})
+
+router.get('/:giftId', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '..public/gift.html'));
+})
+
+export default router;
